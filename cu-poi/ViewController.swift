bf0365a6a0786e4eac28b4ba0fb450fa8ba36929
mguida22 <michael.guida@colorado.edu>
2015-02-03 11:25:25 -0700

    MapKit is up and running

--- cu-poi/ViewController.swift
+++ cu-poi/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let startLocation = CLLocationCoordinate2D(latitude: 40.008180, longitude: -105.267573)
+        
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: startLocation, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(startLocation)
+        annotation.title = "University of Colorado Boulder"
+        annotation.subtitle = "Main Campus"
+        
+        mapView.addAnnotation(annotation)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
