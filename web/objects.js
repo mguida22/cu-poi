470ad390608dda51d57795de7033ef2de6ea28c0
mguida22 <michael.guida@colorado.edu>
2015-05-05 13:59:05 -0600

    Removing unnecessary code

--- web/objects.js
+++ web/objects.js
@@ -12,28 +12,6 @@ function POI(key, name, lat, long, type) {
     this.type = type;
 
     this.coordinate = new google.maps.LatLng(lat, long);
-
-    this.setKey = function(key){
-        this.key = key;
-    }
-
-    this.setName = function(name){
-        this.name = name;
-    }
-
-    this.setLat = function(lat){
-        this.lat = lat;
-        this.coordinate = new google.maps.LatLng(lat, long);
-    }
-
-    this.setLong = function(long){
-        this.long = long;
-        this.coordinate = new google.maps.LatLng(lat, long);
-    }
-
-    this.setType = function(type){
-        this.type = type;
-    }
 }
 
 //MapMarker Objects
